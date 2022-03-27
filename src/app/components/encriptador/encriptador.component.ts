78b4c2fb81ca7707c695a99a2f17c637b65fdb75
Eyder Suárez <eyder.suarez@nivelics.co>
2022-03-26 20:23:56 -0500

    Maquetacion al 70%

---
+++ src/app/components/encriptador/encriptador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encriptador',
+  templateUrl: './encriptador.component.html',
+  styleUrls: ['./encriptador.component.css']
+})
+export class EncriptadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
